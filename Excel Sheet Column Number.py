effb6e05b344e4347557fc6c2e6f2c0f2f40dc70
hecter <hecter654321@gmail.com>
2018-02-24 21:19:20 +0800

    pass testcase: testSample by using ord

--- Excel Sheet Column Number.py
+++ Excel Sheet Column Number.py
@@ -4,11 +4,20 @@ class unitest(unittest.TestCase):
     def testNone(self):
         Input = ""
         Output = 0
-        self.assertEqual(Solution().titleToNumber(Input),Output);
+        self.assertEqual(Solution().titleToNumber(Input),Output)
+    def testSample(self):
+        Input = "AAA"
+        Output = 703
+        self.assertEqual(Solution().titleToNumber(Input),Output)
 
 class Solution():
     def titleToNumber(self, s):
         if s == "":
             return 0
+        Ans = 0
+        for i in range(len(s)):
+            Ans *= 26
+            Ans += ord(s[i]) - ord("A") + 1
+        return Ans
 if __name__ == '__main__':
     unittest.main()
